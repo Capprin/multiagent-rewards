29e1b19942f5ceaf9e2145a78f2afb01d01b3fa9
Capprin Bass <capprin@gmail.com>
2020-11-03 19:29:35 -0800

    Updated bar problem to include Nash equilibria

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from agent import Agent
 
 # input params
 N = 50 #agents
-K = 6 #days
-b = 4 #optimal patrons
+K = 5 #days
+b = 5 #optimal patrons
 weeks = 1000 #iterations
-alpha = 0.5 #monte carlo update
+alpha = 0.2 #monte carlo update
 eps = 0.01 #exploration frequency
-reward_type = 'global' # global, local, difference
+reward_type = 'difference' # global, local, difference
 ci = 0 #difference counterfactual
 
 # payoff for single day(action)
@@ -39,45 +39,59 @@ def get_reward(week, day_num):
   elif reward_type == 'difference':
     return difference_reward(day_num, week)
 
-# computes general alignment (bc all agents have same reward fn)
-def alignment(week):
-  res = 0
-  for i in range(K):
-    mod_week = copy.deepcopy(week)
-    mod_week[i] += 1
-    res += np.sign((get_reward(week, i)-get_reward(mod_week, i))*(system_reward(week)-system_reward(mod_week)))
-  res /= K
-  return res
+# determines whether given result is a nash equilibrium
+def is_nash(agents, week):
+  for agent in agents:
+    # record reward, remove agent
+    last_rew = get_reward(week, agent.day)
+    week[agent.day] -= 1
+    for day in range(len(week)):
+      # skip the day we chose
+      if day == agent.day:
+        continue
+      # temporarily update different day
+      week[day] += 1
+      if get_reward(week, day) > last_rew:
+        return False
+      # undo update
+      week[day] -= 1
+    # reset week
+    week[agent.day] += 1
+  return True
 
-rew_fig = go.Figure()
-# run for ea. reward type
-for reward_type in ['local', 'global', 'difference']:
 
-  # create agents
-  agents = []
-  for i in range(N):
-    agents.append(Agent(K, alpha, eps))
+# create agents
+agents = []
+for i in range(N):
+  agents.append(Agent(K, alpha, eps))
 
-  # game loop
-  rec_rew = []
-  for week in range(weeks):
-    # create bar
-    bar = [0] * K
+# game loop
+rec_rew = []
+nash_eq = []
+for week in range(weeks):
+  # create bar
+  bar = [0] * K
 
-    # all agents take action, update system state
-    for agent in agents:
-      bar[agent.act()] += 1
+  # all agents take action, update system state
+  for agent in agents:
+    bar[agent.act()] += 1
 
-    # assign reward
-    for agent in agents:
-      agent.reward(get_reward(bar, agent.day))
-    
-    # save system parameters
-    rec_rew.append(system_reward(bar))
-  rew_fig.add_trace(go.Scatter(y=rec_rew, name=reward_type.capitalize()))
+  # assign reward
+  for agent in agents:
+    agent.reward(get_reward(bar, agent.day))
+  
+  # save system parameters
+  rec_rew.append(system_reward(bar))
 
+  if is_nash(agents, bar) and not bar in nash_eq:
+    nash_eq.append(bar)
+
+print("Nash Equilibria:")
+print(nash_eq)
+
+for eq in nash_eq:
   hist_fig = go.Figure()
-  hist_fig.add_trace(go.Bar(y=bar))
+  hist_fig.add_trace(go.Bar(y=eq))
   hist_fig.update_layout(
     font_family = 'Computer Modern',
     xaxis_title = 'Night',
@@ -88,6 +102,8 @@ for reward_type in ['local', 'global', 'difference']:
   hist_fig.show()
 
 # plot reward
+rew_fig = go.Figure()
+rew_fig.add_trace(go.Scatter(y=rec_rew))
 rew_fig.update_layout(
   font_family="Computer Modern",
   xaxis_title="Iterations",
